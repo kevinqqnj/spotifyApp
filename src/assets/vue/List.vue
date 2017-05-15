a6d8173f99dde88ad833aabafec9c9f0f725c780
kevinqqnj <ezhqing@gmail.com>
2017-05-15 22:33:46 +0800

    navbar through

--- src/assets/vue/List.vue
+++ src/assets/vue/List.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="list">
+  <f7-page name="list" id="list">
     <f7-navbar>
       <f7-nav-left back-link="Back" sliding></f7-nav-left>
       <f7-nav-center sliding>Results</f7-nav-center>
@@ -7,32 +7,30 @@
         <f7-link icon="icon-bars" open-panel=""></f7-link>
       </f7-nav-right>
     </f7-navbar>
-    <f7-page name="list">
-      <!-- Search bar -->
-      <f7-searchbar cancel-link="Cancel" search-list="#mediaList" placeholder="Search in items" :clear-button="true">
-      </f7-searchbar>
-      <!-- Will be visible if there is no any results found, defined by "searchbar-not-found" class -->
-      <f7-list class="searchbar-not-found">
-        <f7-list-item title="Nothing found"></f7-list-item>
-      </f7-list>
-      <!-- Search-through list -->
-      <f7-list class="searchbar-found" media-list id="mediaList">
-        <f7-list-item swipeout v-for="(item, index) in searchTracks" :key="item.id" :link="'/media/'+index+'/'" :media="'<img src=' + item.album.images[2].url +' >'" :title="item.name" :subtitle="item.artists[0].name" :text="item.album.name">
-          <f7-swipeout-actions right>
-            <f7-swipeout-button>
-              <a class="bg-orange favorite" :data-item="index"><i class="icon fa fa-star fa-2x"></i></a>
-              <a href="#" class="bg-blue share" :data-item="index"><i class="icon fa fa-share fa-2x"></i></a>
-            </f7-swipeout-button>
-          </f7-swipeout-actions>
-          <f7-swipeout-actions left>
-            <f7-swipeout-button>
-              <a href="#" class="bg-green preview" :data-item="index"><i class="icon fa fa-play fa-2x"></i></a>
-            </f7-swipeout-button>
-          </f7-swipeout-actions>
-        </f7-list-item>
-      </f7-list>
-    </f7-page>
-  </div>
+    <!-- Search bar -->
+    <f7-searchbar cancel-link="Cancel" search-list="#mediaList" placeholder="Search in items" :clear-button="true">
+    </f7-searchbar>
+    <!-- Will be visible if there is no any results found, defined by "searchbar-not-found" class -->
+    <f7-list class="searchbar-not-found">
+      <f7-list-item title="Nothing found"></f7-list-item>
+    </f7-list>
+    <!-- Search-through list -->
+    <f7-list class="searchbar-found" media-list id="mediaList">
+      <f7-list-item swipeout v-for="(item, index) in searchTracks" :key="item.id" :link="'/media/'+index+'/'" :media="'<img src=' + item.album.images[2].url +' >'" :title="item.name" :subtitle="item.artists[0].name" :text="item.album.name">
+        <f7-swipeout-actions right>
+          <f7-swipeout-button>
+            <a class="bg-orange favorite" :data-item="index"><i class="icon fa fa-star fa-2x"></i></a>
+            <a href="#" class="bg-blue share" :data-item="index"><i class="icon fa fa-share fa-2x"></i></a>
+          </f7-swipeout-button>
+        </f7-swipeout-actions>
+        <f7-swipeout-actions left>
+          <f7-swipeout-button>
+            <a href="#" class="bg-green preview" :data-item="index"><i class="icon fa fa-play fa-2x"></i></a>
+          </f7-swipeout-button>
+        </f7-swipeout-actions>
+      </f7-list-item>
+    </f7-list>
+  </f7-page>
 </template>
 <script>
 import store from '../../store.js'
--- src/assets/vue/Media.vue
+++ src/assets/vue/Media.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="media">
+  <f7-page name="media" no-toolbar id="media">
     <f7-navbar>
       <f7-nav-left back-link="Back" sliding></f7-nav-left>
       <f7-nav-center sliding>Media Detail</f7-nav-center>
@@ -7,44 +7,41 @@
         <f7-link icon="icon-bars" open-panel=""></f7-link>
       </f7-nav-right>
     </f7-navbar>
-    <f7-page name="media" no-toolbar no-navbar>
-      <f7-block-title></f7-block-title>
-      <f7-block-title>Track Info</f7-block-title>
-      <f7-list>
-        <f7-list-item :media="'<img src=' + searchTracks[mediaId].album.images[2].url +' >'" :title="searchTracks[mediaId].name">
-        </f7-list-item>
-        <f7-list-item>
-          <f7-label>Type</f7-label>
-          <f7-label>{{searchTracks[mediaId].type}}</f7-label>
-        </f7-list-item>
-        <f7-list-item>
-          <f7-label>Artist</f7-label>
-          <f7-label>{{searchTracks[mediaId].artists[0].name}}</f7-label>
-        </f7-list-item>
-      </f7-list>
-      <f7-block-title>Album Info</f7-block-title>
-      <f7-list form>
-        <f7-list-item title="">
-          <f7-label>Album</f7-label>
-          <f7-label>{{searchTracks[mediaId].album.name}}</f7-label>
-        </f7-list-item>
-        <f7-list-item>
-          <f7-label>Popularity Rank</f7-label>
-          <f7-input type="range" min="0" max="100" step="1" v-model="sliderVal" disabled/>
-          <span>&nbsp;&nbsp;</span>
-          <f7-input type="text" disabled value="90" />
-        </f7-list-item>
-      </f7-list>
-      <f7-block-title>Preview</f7-block-title>
-      <f7-block inset>
-        <f7-block inner>
-          <div id="previewAudio">
-            <audio controls preload :src="searchTracks[mediaId].preview_url"></audio>
-          </div>
-        </f7-block>
+    <f7-block-title>Track Info</f7-block-title>
+    <f7-list>
+      <f7-list-item :media="'<img src=' + searchTracks[mediaId].album.images[2].url +' >'" :title="searchTracks[mediaId].name">
+      </f7-list-item>
+      <f7-list-item>
+        <f7-label>Type</f7-label>
+        <f7-label>{{searchTracks[mediaId].type}}</f7-label>
+      </f7-list-item>
+      <f7-list-item>
+        <f7-label>Artist</f7-label>
+        <f7-label>{{searchTracks[mediaId].artists[0].name}}</f7-label>
+      </f7-list-item>
+    </f7-list>
+    <f7-block-title>Album Info</f7-block-title>
+    <f7-list form>
+      <f7-list-item title="">
+        <f7-label>Album</f7-label>
+        <f7-label>{{searchTracks[mediaId].album.name}}</f7-label>
+      </f7-list-item>
+      <f7-list-item>
+        <f7-label>Popularity Rank</f7-label>
+        <f7-input type="range" min="0" max="100" step="1" v-model="sliderVal" disabled/>
+        <span>&nbsp;&nbsp;</span>
+        <f7-input type="text" disabled value="90" />
+      </f7-list-item>
+    </f7-list>
+    <f7-block-title>Preview</f7-block-title>
+    <f7-block inset>
+      <f7-block inner>
+        <div id="previewAudio">
+          <audio controls preload :src="searchTracks[mediaId].preview_url"></audio>
+        </div>
       </f7-block>
-    </f7-page>
-  </div>
+    </f7-block>
+  </f7-page>
 </template>
 <script>
 import store from '../../store.js'
